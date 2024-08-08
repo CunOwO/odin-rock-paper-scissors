bad098909f9b0c06dff02d374009cc9dbf2a2730
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-08 17:13:20 +0700

    Add playRound function
    - Implemented playRound function to determine the winner of a round based on human and computer choices.
    - Made humanChoice parameter case-insensitive.
    - Updated scores and logged round results accordingly.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let compt_choice = Math.floor(Math.random() * 3);
-    switch(compt_choice) {
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice) {
         case 0:
             return "rock";
         case 1:
@@ -9,10 +12,61 @@ function getComputerChoice() {
             return "scissors";
     }
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let user_choice = prompt("Your choice: ");
-    return user_choice;
+    let choice = prompt("Your choice: ");
+    return choice;
 }
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice === "rock") {
+                console.log("Tie!");
+            }
+            else if (computerChoice === "scissors") {
+                humanScore += 1;
+                console.log("You win! Rock beats Scissors");
+            }
+            else if (computerChoice === "paper") {
+                computerScore += 1;
+                console.log("You lose! Paper beats Rock");
+            }
+            break;
+        case "paper":
+            if (computerChoice === "paper") {
+                console.log("Tie!");
+            }
+            else if (computerChoice === "rock") {
+                humanScore += 1;
+                console.log("You win! Paper beats Rock");
+            }
+            else if (computerChoice === "scissors") {
+                computerScore += 1;
+                console.log("You lose! Scissors beats Paper");
+            }
+            break;
+        case "scissors":
+            if (computerChoice === "scissors") {
+                console.log("Tie!");
+            }
+            else if (computerChoice === "paper") {
+                humanScore += 1;
+                console.log("You win! Scissors beats Paper");
+            }
+            else if (computerChoice === "rock") {
+                computerScore += 1;
+                console.log("You lose! Rock beats Scissors");
+            }
+            break;
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
+
