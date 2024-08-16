a00fc4a869a22b8584f6bbf07b13ca39ce434be1
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-16 19:14:12 +0700

    Update Rock-Paper-Scissors game
    - Change <p> elements' IDs to gameScore and gameStatus
    - Add const gameScore and const gameStatus for score and status display
    - Replace alerts in playGame function with gameStatus.textContent
    - Hide gameScore.textContent and gameStatus.textContent after the game finishes
    - Rearrange code structure for better readability

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const gameScore = document.querySelector("#gameScore");
+const gameStatus = document.querySelector("#gameStatus");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", playGame);
+});
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch(choice) {
@@ -13,22 +21,22 @@ function getComputerChoice() {
     }
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", playGame);
-});
-
 function playGame(e) {
     const humanSelection = e.target.textContent;
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection); 
-      
+
+    gameScore.textContent = `Score: ${humanScore} - ${computerScore}`;
     if (humanScore === 5) {
+        gameScore.textContent = "";
+        gameStatus.textContent = "";
         alert(`You win! Your score: ${humanScore} - ${computerScore}`);
         humanScore = 0;
         computerScore = 0;
     }
     else if (computerScore === 5) {
+        gameScore.textContent = "";
+        gameStatus.textContent = "";
         alert(`You lose... Your score: ${humanScore} - ${computerScore}`);
         humanScore = 0;
         computerScore = 0;
@@ -41,41 +49,41 @@ function playRound(humanChoice, computerChoice) {
     switch (humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
-                alert("Tie!");
+                gameStatus.textContent = "Tie!";
             }
             else if (computerChoice === "scissors") {
                 humanScore += 1;
-                alert("You win! Rock beats Scissors");
+                gameStatus.textContent = "You win! Rock beats Scissors";
             }
             else if (computerChoice === "paper") {
                 computerScore += 1;
-                alert("You lose! Paper beats Rock");
+                gameStatus.textContent = "You lose! Paper beats Rock";
             }
             break;
         case "paper":
             if (computerChoice === "paper") {
-                alert("Tie!");
+                gameStatus.textContent = "Tie!";
             }
             else if (computerChoice === "rock") {
                 humanScore += 1;
-                alert("You win! Paper beats Rock");
+                gameStatus.textContent = "You win! Paper beats Rock";
             }
             else if (computerChoice === "scissors") {
                 computerScore += 1;
-                alert("You lose! Scissors beats Paper");
+                gameStatus.textContent = "You lose! Scissors beats Paper";
             }
             break;
         case "scissors":
             if (computerChoice === "scissors") {
-                alert("Tie!");
+                gameStatus.textContent = "Tie!";
             }
             else if (computerChoice === "paper") {
                 humanScore += 1;
-                alert("You win! Scissors beats Paper");
+                gameStatus.textContent = "You win! Scissors beats Paper";
             }
             else if (computerChoice === "rock") {
                 computerScore += 1;
-                alert("You lose! Rock beats Scissors");
+                gameStatus.textContent = "You lose! Rock beats Scissors";
             }
             break;
     }
